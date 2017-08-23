51615bc91a62ba622454dfde78c353ce494f8a86
sethcarrino <sethcarrino@gmail.com>
2017-08-23 11:48:50 -0400

    Initial Commit - created all boilerplate for project - added data

---
+++ src/components/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import '../styles/App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="title">
+          <div className="my-header">
+            <h1>Earthquakes!</h1>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
